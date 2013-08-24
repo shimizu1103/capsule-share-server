9fe6555c0cacb0da2206305a840cbc68c9ec1378
Kazuya Arakawa <arakawa@vyw.jp>
2013-08-24 17:51:11 +0000

    api auth_token発行のみ

--- app/controllers/api/users/sessions_controller.rb
+++ app/controllers/api/users/sessions_controller.rb
@@ -1,2 +1,27 @@
-class Api::Users::SessionsController < ApplicationController
+class Api::Users::SessionsController < Devise::SessionsController
+  include ApiCommon
+
+  skip_before_action :verify_authenticity_token
+
+  # http://matteomelani.wordpress.com/2011/10/17/authentication-for-mobile-devices/
+  def create
+    email = params[:email]
+    password = params[:password]
+
+    if email.nil? || password.nil?
+      render json: failed("invalid parameters")
+      return
+    end
+
+    @user = User.find_for_database_authentication(email: email.downcase)
+
+    if @user.nil? || !@user.valid_password?(password)
+      render json: failed("unauthorized")
+      return
+    end
+
+    @user.reset_authentication_token!
+
+    render json: { result: 0, auth_token: @user.authentication_token }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 CapsuleShare::Application.routes.draw do
   root "home#index"
 
-  # devise_for :users, only: [:sessions, :registrations], controllers: { sessions: "users/sessions", registrations: "users/registrations" }, format: false
   devise_for :users, module: "users", format: false
 
   # scope :api do
   #   devise_for :users, only: [:sessions, :registrations], controllers: {sessions: 'api/users/sessions', registrations: 'api/users/registrations'}, format: false
   # end
+
+  # namespace :api, format: false do
+  namespace :api, defaults: { format: :json } do
+    devise_scope :user do
+      post "users/sign_in" => "users/sessions#create"
+    end
+  end
 end
