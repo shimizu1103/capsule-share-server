7e8bca36056f5afc8d3e598bb6667a78c0817d6c
Ubuntu <ubuntu@ip-172-31-31-138.ap-northeast-1.compute.internal>
2013-08-24 08:05:11 +0000

    rails new & set .gitignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CapsuleShare::Application.config.secret_key_base = '517feef1776bba5056b3ad4ba11572c1c994ab8670697d57a5e6a4b7251f0ca319d09cb7201420a631fc5501847788c8bee8ee38a5bee044b38726cdb9e1731c'
