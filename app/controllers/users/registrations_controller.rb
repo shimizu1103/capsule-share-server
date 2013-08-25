9b1d4c1d2ad67c97904c3662b64cac026de01b3d
Kazuya Arakawa <arakawa@vyw.jp>
2013-08-25 06:56:32 +0000

    メール認証追加

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,2 +1,18 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+  before_action :mailer_set_url_options, if: -> { ENV['RAILS_ENV'] == "development" }
+  before_filter :configure_permitted_parameters
+
+  private
+
+  def mailer_set_url_options
+    ActionMailer::Base.default_url_options[:host] = request.host_with_port
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :password_confirmation, :name) } 
+    devise_parameter_sanitizer.for(:account_updatel) { |u| u.permit(:email, :password, :password_confirmation, :current_password, :name) } 
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,8 @@ CapsuleShare::Application.routes.draw do
 
   devise_for :users, module: "users", format: false
 
-  # scope :api do
-  #   devise_for :users, only: [:sessions, :registrations], controllers: {sessions: 'api/users/sessions', registrations: 'api/users/registrations'}, format: false
-  # end
+  get "my" => "my#index"
 
-  # namespace :api, format: false do
   namespace :api, defaults: { format: :json } do
     devise_scope :user do
       post "users/sign_in" => "users/sessions#create"
