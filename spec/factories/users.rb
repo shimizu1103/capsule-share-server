12f045b4d48d2f4df95154f9b01009015bf18de1
Kazuya Arakawa <arakawa@vyw.jp>
2013-09-01 10:22:26 +0000

    factories:users作成

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,12 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
+
   factory :user do
+    sequence(:name) { |n| "テスト#{n}" }
+    sequence(:email) { |n| "person-#{n}@example.com" }
+    password "1111"
+    password_confirmation "1111"
+    confirmed_at Time.now
   end
 end
