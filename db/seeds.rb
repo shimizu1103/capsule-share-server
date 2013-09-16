d41900d36c370495fb76e4a1878483a572f147b1
Kazuya Arakawa <arakawa@vyw.jp>
2013-09-16 07:43:38 +0000

    seed:users/user_users/friend_requests

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+friend_requests_to = []
+friend_requests_from = []
+2.times do |i|
+  friend_requests_to << FactoryGirl.create(:user)
+end
+2.times do |i|
+  friend_requests_from << FactoryGirl.create(:user)
+end
+
+users = [
+  FactoryGirl.create(:arakawa),
+  FactoryGirl.create(:kawasaki),
+  FactoryGirl.create(:shimizu),
+]
+
+users.each do |user|
+  users.each do |to_user|
+    next if user == to_user
+    user.user_users.create friend_user_id: to_user.id
+  end
+
+  friend_requests_to.each do |friend_request_to|
+    user.friend_requests_from_me.create friend_user_id: friend_request_to.id
+  end
+
+  friend_requests_from.each do |friend_request_from|
+    user.friend_requests_to_me.create user_id: friend_request_from.id
+  end
+end
