9b8cb51a4512fab9f6330feb714119ea9cf066b8
Ubuntu <ubuntu@ip-172-31-30-197.us-west-2.compute.internal>
2013-09-21 08:13:00 +0000

    ユーザー検索機能追加

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,24 @@
+class Api::UsersController < ApplicationController
+
+  skip_before_action :verify_authenticity_token
+  before_action :authenticate_user!
+
+    def search
+
+#binding.pry
+    if params[:user].blank? || params[:user][:email].blank?
+       render json: result_failed
+       return
+    end
+       
+     @users = User.where(email: params[:user][:email]).first
+
+        if @users.blank?
+            render json: {result: 1}
+        else
+            render json: { result: 0, users: [id: @users.id, email: @users.email, name: @users.name] }
+        end
+
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,7 @@ CapsuleShare::Application.routes.draw do
       post "users/sign_in" => "users/sessions#create"
     end
     post "friends/create", to: "friends#create"
+    post "users/search", to: "users#search"
   end
+    #post "users/search" => "api/users#search"
 end
