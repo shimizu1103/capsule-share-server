f013222bd8b200d25be57ef17573e4fe57a3f4d7
Kazuya Arakawa <arakawa@vyw.jp>
2013-09-21 12:04:57 +0000

    route:upload_files追加

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,14 @@ CapsuleShare::Application.routes.draw do
 
   get "my" => "my#index"
 
+  resources :upload_files
+
   namespace :api, defaults: { format: :json } do
     devise_scope :user do
       post "users/sign_in" => "users/sessions#create"
     end
     post "friends/create", to: "friends#create"
     post "users/search", to: "users#search"
+    post "upload_files/create", to: "upload_files#create"
   end
 end
