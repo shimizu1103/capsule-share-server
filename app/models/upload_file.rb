450ddbc8c974ff15a0d0098e06c7c215de2aef56
Kazuya Arakawa <arakawa@vyw.jp>
2013-09-22 18:50:59 +0900

    file_nameのデフォルト設定

--- app/models/upload_file.rb
+++ app/models/upload_file.rb
@@ -2,4 +2,15 @@ class UploadFile < ActiveRecord::Base
   belongs_to :user
   
   mount_uploader :owned_file, OwnedFileUploader
+
+  before_validation :set_file_name
+
+  private
+
+  # file_nameが指定なしの場合はアップロードされたファイル名とする
+  def set_file_name
+    if self.file_name.blank? && owned_file.present?
+      self.file_name = owned_file.sanitized_file.original_filename
+    end
+  end
 end
